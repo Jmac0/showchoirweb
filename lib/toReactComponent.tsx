8ab83827682878cf43e99d8cf245a861df8d22dc
JamieMac <Jamiejmackenzie@gmail.com>
2022-10-29 17:53:38 +0100

    Amended content model to get button names for menu

--- lib/toReactComponent.tsx
+++ lib/toReactComponent.tsx
@@ -1,5 +1,5 @@
 import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
-
+// styles the ul from contentful
 export const formatOptions = {
   renderNode: {
     [BLOCKS.UL_LIST]: (node: any, children: any[]) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,41 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import { getHomePageData } from "../lib/getPages";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { formatOptions } from "../lib/toReactComponent";
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { getHomePageData, getPageData } from '../lib/getPages';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { formatOptions } from '../lib/toReactComponent';
 
 export async function getStaticProps() {
-  const data = await getHomePageData();
+  const homePageData = await getHomePageData();
   const {
     fields: { title },
-  } = data;
-  // const bodyContent = data.fields.content;
+  } = homePageData;
   const {
     fields: { content },
-  } = data;
-  return { props: { title, content } };
+  } = homePageData;
+  /*get paths for each page from contentful*/
+  const res = await getPageData();
+  const { items } = res;
+  const linkData = items.map(
+    (item: { fields: { slug: string; displayText: string } }) => {
+      return { slug: item.fields.slug, displayText: item.fields.displayText };
+    },
+  );
+  //onst paths = slugs.map((s: { slug: any }) => s);
+  console.log(linkData);
+
+  return { props: { title, content, linkData } };
 }
 
 type Props = {
   title: string;
   content: {};
+  linkData: [{ slug: string; displayText: string }];
 };
-const Home: NextPage<Props> = ({ title, content }) => {
-  const [bodyTxt, setBodyTxt] = useState("");
-  // convert contentful object to html ritch text
+const Home: NextPage<Props> = ({ title, content, linkData }) => {
+  const [bodyTxt, setBodyTxt] = useState('');
+  // convert contentful object to html rich text
   // @ts-ignore
   const bodyHtml = documentToReactComponents(content, formatOptions);
   useEffect(() => {
@@ -32,7 +44,7 @@ const Home: NextPage<Props> = ({ title, content }) => {
   }, []);
 
   return (
-    <div>
+    <>
       <Head>
         <title>{title}</title>
         <meta
@@ -41,11 +53,27 @@ const Home: NextPage<Props> = ({ title, content }) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav>
+        {/*desktop nav container*/}
+        <div className="hidden h-28 from-lightGold flex-row w-full justify-center items-center bg-gradient-to-b to-gold md:flex">
+          <Link href="/">
+            <a className="text-lightBlack font-heading px-3 hover:underline text-2xl">
+              Home
+            </a>
+          </Link>
 
+          {linkData.map((path) => (
+            <Link key={path.slug} href={path.slug}>
+              <a className="text-lightBlack font-heading px-3 hover:underline text-2xl">
+                {path.displayText}
+              </a>
+            </Link>
+          ))}
+        </div>
+      </nav>
       <main>{bodyTxt}</main>
-
       <footer></footer>
-    </div>
+    </>
   );
 };
 
