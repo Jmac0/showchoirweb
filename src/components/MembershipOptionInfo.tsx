3451ab41e44cbca09cf8f70e691077b0fb84993f
JamieMac <Jamiejmackenzie@gmail.com>
2023-02-28 11:28:34 +0000

    Make MembershipOptionInfo responsive

--- src/components/MembershipOptionInfo.tsx
+++ src/components/MembershipOptionInfo.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const MembershipOptionInfo = ({ markdown, navigateTo }: Props) => {
   return (
     <div
-      className="flex w-11/12 h-full  m-2 flex-col rounded-md border-2 border-lightGold  p-2
+      className="flex w-11/12 md:h-full  m-2 flex-col rounded-md border-2 border-lightGold  p-2
 	  pl-5 text-gray-50 md:w-2/3  lg:w-1/3 "
     >
       <ReactMarkdown>{markdown}</ReactMarkdown>
