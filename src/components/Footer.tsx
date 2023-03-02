82f3d8dd862257373f6133cd85edbd0000f5b9cd
JamieMac <Jamiejmackenzie@gmail.com>
2023-03-02 16:17:19 +0000

    Add margin to top of [slug] to accommodate menu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function Footer() {
   return (
-    <footer className="text-white h-80 mt-2.5 bg-lightGold">footer</footer>
+    <footer className="text-white h-40 mt-2.5 bg-lightGold">footer</footer>
   );
 }
