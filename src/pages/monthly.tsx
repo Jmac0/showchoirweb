a233831ad214d1431f2ff3c967ef268598a2c969
JamieMac <Jamiejmackenzie@gmail.com>
2023-03-13 21:16:54 +0000

    Add authorization_url to useHttp, redirect to Go cardless

--- src/pages/monthly.tsx
+++ src/pages/monthly.tsx
@@ -1,19 +1,24 @@
+import React, { useEffect } from 'react';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { Footer } from '../components/Footer';
 import NewMemberSignUpForm from '../components/NewMemberSignUpForm/NewMemberSignUpForm';
-import {useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectUrls } from '../features/urlSlice';
 import Head from 'next/head';
 import { Nav } from '../components/Nav';
-import useHttp from "../components/hooks/useHttp";
+import useHttp from '../components/hooks/useHttp';
 const MonthlyMembershipDetail: NextPage = () => {
+  const router = useRouter();
   const pathData = useSelector(selectUrls);
   const {
     loading,
     message,
     setMessage,
     setLoading,
+    setResponseData,
     sendRequest,
+    responseData,
     showUserMessage,
     setShowUserMessage,
     isErrorMessage,
@@ -23,10 +28,22 @@ const MonthlyMembershipDetail: NextPage = () => {
     method: 'POST',
     withCredentials: false,
   });
+  useEffect(() => {
+    if (responseData.authorisation_url) {
+      setMessage('Redirecting');
+      router.push(responseData.authorisation_url);
+    }
+
+    return () => {
+      setResponseData({ authorisation_url: '' });
+    };
+  }, [responseData.authorisation_url, setMessage, router, setResponseData]);
 
   const submitForm = async (data: any) => {
-	 await sendRequest({data})
+    await sendRequest({ data });
   };
+
+  console.log('URL:', responseData);
   return (
     <>
       <Head>
@@ -42,8 +59,14 @@ const MonthlyMembershipDetail: NextPage = () => {
         <Nav pathData={pathData} />
         {/*Add margin top to accommodate menu*/}
         <div className="mt-16 flex flex-col items-center">
-	  {/*Props for message component passed down to parent  */}
-          <NewMemberSignUpForm  loading={loading} submitForm={submitForm} isErrorMessage={isErrorMessage} message={message} showUserMessage={showUserMessage} />
+          {/*Props for message component passed down to parent  */}
+          <NewMemberSignUpForm
+            loading={loading}
+            submitForm={submitForm}
+            isErrorMessage={isErrorMessage}
+            message={message}
+            showUserMessage={showUserMessage}
+          />
         </div>
         <Footer />
       </div>
